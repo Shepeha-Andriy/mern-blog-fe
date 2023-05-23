c97de4729fa83cde4ba49cac6d308f7ab4335616
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-05-23 17:37:06 +0300

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from 'react-router-dom'
 import {checkIsAuth, logout} from '../redux/features/auth/authSlice'
