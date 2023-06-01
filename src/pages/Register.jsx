b841ba6c0a9e6427c81959d82d411d7c50238449
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-06-01 18:28:12 +0300

    react form hook

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,14 +4,17 @@ import { Link, useNavigate } from "react-router-dom";
 import {checkIsAuth} from '../redux/features/auth/authSlice'
 import { registerUser } from "../redux/features/auth/authSlice";
 import { toast } from "react-toastify";
+import { useForm } from 'react-hook-form'
 
 export const Register = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const { status } = useSelector(state => state.auth)
   const dispatch = useDispatch()
   const isAuth = useSelector(checkIsAuth)
   const navigate = useNavigate()
+
+  const { register, formState: { errors, isValid }, handleSubmit, reset } = useForm({
+    mode: 'onBlur'
+  })
 
   useEffect(() => {
     if (status) {
@@ -23,11 +26,11 @@ export const Register = () => {
     }
   }, [status, isAuth, navigate])
 
-  const handleSubmit = () => {
+  const handleSubmit2 = (data) => {
     try {
+      const {username, password} = data
       dispatch(registerUser({ username, password }))
-      setPassword('')
-      setUsername('')
+      reset()
     } catch (error) {
       console.log(error)
     }
@@ -39,20 +42,33 @@ export const Register = () => {
 
       <label className="text-xs text-gray-400">
         Username:
-        <input type="text" placeholder="username" value={username} onChange={(e) => {setUsername(e.target.value)}}
-          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-500">
-        </input>
+        <input placeholder="username" {...register('username', {
+          required: 'username is required',
+          minLength: {value: 5, message: 'min length is 5'}
+        })}
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-500"
+        ></input>
+        <div style={{marginTop: '3px'}}>
+          {errors?.username && <p>{ errors?.username?.message }</p>}
+        </div>
       </label>
 
       <label className="text-xs text-gray-400">
         Password:
-        <input type="password" placeholder="password" value={password} onChange={(e) => {setPassword(e.target.value)}}
-          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-500">
-        </input>
+        <input type="password" placeholder="password" {...register('password', {
+          required: 'username is required',
+          minLength: {value: 6, message: 'min length is 5'},
+          maxLength : {value: 16, message: 'max length is 16'}
+        })}
+          className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-500"
+        ></input>
+        <div style={{marginTop: '3px'}}>
+          {errors?.password && <p>{ errors?.password?.message }</p>}
+        </div>
       </label>
 
       <div className="flex gap-8 justify-center mt-4">
-        <button type="submit" onClick={handleSubmit}
+        <button type="submit" disabled={!isValid} onClick={handleSubmit(handleSubmit2)}
           className="flex justify-center items-center text-xs bg-gray-600 text-white rounded-sm py-2 px-4">Register
         </button>
         <Link
@@ -63,10 +79,11 @@ export const Register = () => {
       </div>
 
       <Link
-          className="flex justify-center items-center text-xs text-white"
-          to={'/'}>
-          go back
-        </Link>
+        className="flex justify-center items-center text-xs text-white"
+        to={'/'}
+      >
+        go back
+      </Link>
       
    </form>
   )
